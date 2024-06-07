478895d7a53aeb0dca3c0fb30a02c5f8b2ba3e7e
Aaron Snowberger <jekkilekki@gmail.com>
2024-06-07 12:33:06 +0900

    Almost done WIP

---
+++ Chp13/grade.h
@@ -0,0 +1,13 @@
+#pragma once
+// grade.h
+
+#ifndef GUARD_grade_h
+#define GUARD_grade_h
+
+#include <list>
+#include <vector>
+
+double grade(double, double, double);
+double grade(double, double, vector<double>&);
+
+#endif
